7089ef3e65cda5208f278614f8a805542c6dd51d
Fay Marie <samsongreen72@gmail.com>
2020-08-02 19:22:14 -0400

    Added a saveNote function to update the text value of the note

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ function uuidv4() {
   });
 }
 
+
+// CREATE
 function newNote() {
 	const note = {
 		id: uuidv4(),
@@ -21,23 +23,53 @@ function newNote() {
 	console.log(notes);
 }
 
+// DELETE
 function deleteNote(id) {
 	notes = notes.filter(n => n.id !== id);
 	renderNotes();
 	console.log('yes')
 }
 
+// function saveNote(id) {
+// 	const note = notes.find(n => n.id == id);
+// 	const newText = document.getElementById('text-' + note.id).value;
+
+// 	note.text = newText;
+// 	const newNote = Object.assign({}, note);
+
+// 	renderNotes();
+// }
+
+function saveNote(id) {
+	const note = notes.find(n => n.id == id);
+	const newText = document.getElementById('text-' + note.id).value;
+	const newNote = {
+		...note,
+		text: newText
+	};
+
+	notes = notes.map(n => n.id === id ? newNote : n)
+	renderNotes();
+	console.log(newNote);
+}
+
 function createNoteTemplate(note) {
 	return `<div class="note-card">
 				<button onclick="deleteNote('${note.id}')">X</button>
-				<button>save</button>
+				<button onclick="saveNote('${note.id}')">save</button>
 				<p>${note.id}</p>
+				<form>
+					<textarea id="text-${note.id}" placeholder="Take a note...">${note.text}</textarea>
+				</form>
 			</div>`
 }
 
+// READ
 function renderNotes() {
 	const notehtml = notes.map(note => createNoteTemplate(note));
 	notesWrapper.innerHTML = notehtml;
 }
 
 noteButton.addEventListener("click", newNote);
+
+// Need edit and update functions
